e05cade35a40d3d6dc033658b4029da2f173bada
matheut <matheus.teixeira@tjpe.jus.br>
2026-04-20 11:45:29 -0300

    Initial commit

---
+++ auth-service/src/main/java/com/finconnect/auth_service/configuration/security/AuthServiceApplication.java
@@ -0,0 +1,13 @@
+package com.finconnect.auth_service.configuration.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthServiceApplication.class, args);
+	}
+
+}
