6466922a1f9944428dabd03987dfd0b5ac213e47
matheut <matheus.teixeira@tjpe.jus.br>
2026-04-28 10:53:55 -0300

    Feat: Implementação do gateway e ajustes no auth service

--- auth-service/src/main/resources/db/migration/V1__create_table_users.sql
+++ auth-service/src/main/resources/db/migration/V1__create_table_users.sql
@@ -4,7 +4,7 @@ CREATE TABLE users (
     cpf VARCHAR(11) NOT NULL UNIQUE,
     email VARCHAR(100) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL,
-    roles VARCHAR(50) NOT NULL
+    roles text[] NOT NULL
 );
 
 CREATE UNIQUE INDEX idx_user_cpf ON users(cpf);
