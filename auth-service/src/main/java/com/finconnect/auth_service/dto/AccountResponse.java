d91031e94e995d8c7cdf746b93997802c97ab222
matheut <matheus.teixeira@tjpe.jus.br>
2026-05-19 10:54:55 -0300

    Feat: Documenting auth endpoints

--- auth-service/src/main/java/com/finconnect/auth_service/dto/AccountResponse.java
+++ auth-service/src/main/java/com/finconnect/auth_service/dto/AccountResponse.java
@@ -2,10 +2,36 @@ package com.finconnect.auth_service.dto;
 
 import java.math.BigDecimal;
 import com.finconnect.auth_service.feign.Status;
+import io.swagger.v3.oas.annotations.media.Schema;
 
+@Schema(description = "The account response returned in the sign up")
 public record AccountResponse (
+
+    @Schema(
+        description = "user's account agency",
+        example = "0000-1",
+        requiredMode = Schema.RequiredMode.REQUIRED
+    )
     String agency,
+
+    @Schema(
+        description = "user's account number",
+        example = "0000-1",
+        requiredMode = Schema.RequiredMode.REQUIRED
+    )
     String accountNumber,
+
+    @Schema(
+        description = "the account balance",
+        example = "0.0",
+        requiredMode = Schema.RequiredMode.REQUIRED
+    )
     BigDecimal balance,
+
+    @Schema(
+        description = "the account status",
+        example = "ACTIVE | BLOCKED",
+        requiredMode = Schema.RequiredMode.REQUIRED
+    )
     Status status
 ) {}
--- auth-service/src/main/java/com/finconnect/auth_service/dto/EmailFromCpfResponse.java
+++ auth-service/src/main/java/com/finconnect/auth_service/dto/EmailFromCpfResponse.java
@@ -1,5 +1,14 @@
 package com.finconnect.auth_service.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@Schema(description = "The email extracted from user's cpf")
 public record EmailFromCpfResponse(
+
+    @Schema(
+        description = "user's email",
+        example = "test@test.com",
+        requiredMode = Schema.RequiredMode.REQUIRED
+    )
     String email
 ) {}
