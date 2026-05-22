d68b46c095912851e5bb59cac341feb3b1ffd494
matheut <matheus.teixeira@tjpe.jus.br>
2026-05-22 08:17:23 -0300

    Fixed: removed swagger ui from notification-service

--- notification-service/src/main/java/com/finconnect/notification_service/dto/SendEmailRequest.java
+++ notification-service/src/main/java/com/finconnect/notification_service/dto/SendEmailRequest.java
@@ -1,30 +1,7 @@
 package com.finconnect.notification_service.dto;
 
-import io.swagger.v3.oas.annotations.media.Schema;
-import jakarta.validation.constraints.Email;
-
-@Schema(description = "Request to send an email")
 public record SendEmailRequest(
-
-    @Schema(
-        description = "email destination",
-        example = "destination@test.com",
-        requiredMode = Schema.RequiredMode.REQUIRED
-    )
-    @Email
     String destination,
-
-    @Schema(
-        description = "Email's subject",
-        example = "Subject",
-        requiredMode = Schema.RequiredMode.REQUIRED
-    )
     String subject,
-
-    @Schema(
-        description = "Email's message",
-        example = "Message",
-        requiredMode = Schema.RequiredMode.REQUIRED
-    )
     String message
 ) {}
